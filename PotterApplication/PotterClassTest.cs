85df850e4998801ab0d034fb1379a5f0d1837b61
Olga Cardenas <oh.cardenas@gmail.com>
2014-05-09 12:30:01 -0500

    imported monodevelop project

---
+++ PotterApplication/PotterClassTest.cs
@@ -0,0 +1,13 @@
+using System;
+using PotterLib;
+
+namespace PotterApplication
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
